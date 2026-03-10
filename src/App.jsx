cccaa301363bcd260c5670d34fe8f6ab15f3330e
elprofedeventas <elprofedeventas@gmail.com>
2026-03-09 20:54:17 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -478,7 +478,7 @@ function Dashboard() {
                           )}
                         </div>
                         )
-                      })}
+                      })
                     )}
                   </div>
                 )}
